65d480635537c351cb45f975d6ba92d7c1d72da4
spausek <31174081+spausek@users.noreply.github.com>
2018-01-05 14:45:56 -0800

    Giphs appear on button click
    Added a function that populates a div with gifs from the api

--- assets/java.js
+++ assets/java.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 var giphArray = ['Athens', 'Sponge Bob','Homer Simpson', 'Crying Jordan', 'Julius Caesar', 'Pokemon', 'Gaddafi'];
 var APIKEY = '&api_key=o0mepy6dhm835wqRWR5D9FADRYhZNSGg'
 
+$(document).on('click', '.dank-button', showGifs);
 
 function createButtons () {
 	//$('.button-div').empty();
@@ -12,6 +13,7 @@ function createButtons () {
 		gifButton.attr('data-name', giphArray[i]);
 		gifButton.text(giphArray[i]);
 		$('.button-div').append(gifButton);	
+		//console.log(gifButton.attr('data-name', giphArray[i]));
 
 	}
 }
@@ -28,6 +30,26 @@ function showGifs (){
 
 	.done(function(response){
 		console.log(response);
+		$('.giph-display').empty();
+		var  queryResult = response.data
+		if (queryResult === ''){
+			alert('Sorry, no dank gifs for the search');
+		}
+
+		for (var i = 0; i < queryResult.length; i++) {
+			var newGifDiv = $('<div>');
+			newGifDiv.addClass('generatedGif');
+			var rating = $('<p>').text(queryResult[i].rating);
+			console.log(queryResult[i].rating);
+			newGifDiv.append(rating);
+			var newGifImage = $('<img>');
+			newGifImage.attr('src', queryResult[i].images.fixed_height_small_still.url);
+			newGifImage.attr('data-still', queryResult[i].images.fixed_height_small_still.url);
+			newGifImage.attr('data-state', 'still');
+			newGifDiv.append(newGifImage);
+			$('.giph-display').append(newGifDiv);
+			} 
+	
 
 	});
 
@@ -38,7 +60,7 @@ function showGifs (){
 
 
 createButtons ();
-showGifs ();
+//showGifs ();
 
 
 
